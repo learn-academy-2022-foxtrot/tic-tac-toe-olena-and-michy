26506456e32bf588d31da4b4944790e63885d379
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-09-27 16:30:16 -0700

    I can click on a restart button that will clear the game board.

--- src/App.js
+++ src/App.js
@@ -8,23 +8,51 @@ const App = () => {
   const [currentPlayer, setCurrentPlayer] = useState("❌")
 
   const handleGamePlay = (clickedSquare) => {
-    let updateBoard = [...squares]
-console.log(clickedSquare)
+  console.log(clickedSquare)
+  
   if (squares[clickedSquare] === null) {
-    //updateBoard[clickedSquare] = "❌"
-    //setSquares(updateBoard)
-    setCurrentPlayer(currentPlayer)
+    //const updateSquare = [...squares]
+    squares[clickedSquare] = currentPlayer
+    setSquares(squares)
+    setCurrentPlayer (currentPlayer==="❌" ? "⭕️" : "❌")
+    //setCurrentPlayer(currentPlayer)
   }
   }
+  
+  let winner = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    for (let i=0; i < winner.length; i++) {
+      const [a, b, c] = winner[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+         alert("You Win!")
+        //  let status
+        //  if (winner) {
+        //   status = `Winner: ${winner}`;
+        //  } else {
+        //   status = 'Next player: ' + ("❌" ? "⭕️" : "❌")
+        //  }
+      }  
+    }
 
-  return (
+   const handleReset = () => {
+    setSquares(Array(9).fill(null))
+   }
+    return (
     <>
       <h1>Tic Tac Toe</h1>
       <div className="board-game">
-        {squares.map((squares, index) => {
+        {squares.map((square, index) => {
           return (
             <Square 
-              squares={squares}
+              square={square}
               index={index} 
               key={index}
               handleGamePlay={handleGamePlay} />
@@ -32,6 +60,10 @@ console.log(clickedSquare)
           )
         })}
 
+      </div>
+      {/* //<div className="status">{status}</div> */}
+      <div className="button-section">
+        <button className="reset-button" onClick={handleReset}>Restart Game</button>
       </div>
       
     </>
